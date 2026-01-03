4d1a514cf48f084bd3727ad981fd03581afd3403
rojojun <zoloman316@gmail.com>
2026-01-03 23:28:42 +0900

    WebClient 사용

---
+++ src/test/java/com/rojojun/webflux/WebClientTest.java
@@ -0,0 +1,33 @@
+package com.rojojun.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+public class WebClientTest {
+    private WebClient webClient = WebClient.builder().build();
+
+    @Test
+    public void testWebClient() {
+        Flux<Integer> integerFlux = webClient.get()
+                .uri("http://localhost:8080/reactive/onenine/flux")
+                .retrieve()
+                .bodyToFlux(Integer.class);
+
+        integerFlux.subscribe(data -> {
+            System.out.println(LocalDateTime.now() + "Thread : " + Thread.currentThread().getName());
+            System.out.println("Value : " + data);
+        });
+        System.out.println("END");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+
+        }
+    }
+}
