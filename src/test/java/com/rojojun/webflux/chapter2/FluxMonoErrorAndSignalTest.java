606ccc447ee5cf6256b4cb7d5a3bf29d254a4162
rojojun <zoloman316@gmail.com>
2026-01-05 23:28:20 +0900

    시그널과 예외처리 #2

--- src/test/java/com/rojojun/webflux/chapter2/FluxMonoErrorAndSignalTest.java
+++ src/test/java/com/rojojun/webflux/chapter2/FluxMonoErrorAndSignalTest.java
@@ -2,6 +2,7 @@ package com.rojojun.webflux.chapter2;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public class FluxMonoErrorAndSignalTest {
 
@@ -12,5 +13,34 @@ public class FluxMonoErrorAndSignalTest {
                 .doOnComplete(() -> System.out.println("Stream is finished"))
                 .doOnError(error -> System.out.println("error = " + error))
                 .subscribe(data -> System.out.println("data = " + data));
+    }
+
+    @Test
+    public void testFluxMonoError() {
+        try {
+            Flux.just(1, 2, 3, 4)
+                    .map(data -> {
+                        if (data == 3) {
+                            throw new RuntimeException();
+                        }
+
+                        return data * 2;
+                    })
+                    .subscribe(data -> System.out.println("data = " + data));
+        } catch (Exception e) {
+            System.out.println("에러 발생");
+        }
+    }
+
+    @Test
+    public void testFluxMonoDotError() {
+        Flux.just(1, 2, 3, 4)
+                .flatMap(data -> {
+                    if (data != 3) {
+                        return Mono.just(data);
+                    } else {
+                        return Mono.error(new RuntimeException());
+                    }
+                }).subscribe(data -> System.out.println("data = " + data));
     }
 }
